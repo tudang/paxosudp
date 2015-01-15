8d2011794a640a74cb28aa84008c3c0b697a9ccb
Tu Dang <danghu@usi.ch>
2015-01-15 22:11:56 +0100

    use udptest.sh script to run experiments on fedora ndoes

--- scripts/udptest.sh
+++ scripts/udptest.sh
@@ -10,39 +10,37 @@ OST="$2"
 
 BWM_DIR="/home/danghu/log-bwm"
 EXEC_DIR="/home/danghu/paxosudp/build/sample"
+CONF_DIR="/home/danghu/paxosudp/scripts"
 #start acceptors & proposer
-ssh node70 "nohup $EXEC_DIR/acceptor 0 $EXEC_DIR/paxos.conf > experiment_log/acceptor0.log 2>&1 &"
-ssh node71 "nohup $EXEC_DIR/acceptor 1 $EXEC_DIR/paxos.conf > experiment_log/acceptor1.log 2>&1 &"
-ssh node72 "nohup $EXEC_DIR/acceptor 2 $EXEC_DIR/paxos.conf > experiment_log/acceptor2.log 2>&1 &"
-ssh node74 "nohup $EXEC_DIR/proposer 0 $EXEC_DIR/paxos.conf > experiment_log/proposer0.log 2>&1 &"
+ssh host81 "nohup $EXEC_DIR/acceptor 0 $CONF_DIR/paxos.conf > experiment_log/acceptor0.log 2>&1 &"
+ssh host82 "nohup $EXEC_DIR/acceptor 1 $CONF_DIR/paxos.conf > experiment_log/acceptor1.log 2>&1 &"
+ssh host83 "nohup $EXEC_DIR/acceptor 2 $CONF_DIR/paxos.conf > experiment_log/acceptor2.log 2>&1 &"
+ssh host85 "nohup $EXEC_DIR/proposer 0 $CONF_DIR/paxos.conf > experiment_log/proposer0.log 2>&1 &"
 
 #start bwm-ng on acceptors & proposer
-ssh node70 "nohup bwm-ng $EXEC_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor0.csv 2>&1 &"
-ssh node71 "nohup bwm-ng $EXEC_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor1.csv 2>&1 &"
-ssh node72 "nohup bwm-ng $EXEC_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor2.csv 2>&1 &"
-ssh node74 "nohup bwm-ng $EXEC_DIR/bwm-ng.conf > $BWM_DIR/$N/proposer0.csv 2>&1 &"
-            nohup bwm-ng $EXEC_DIR/bwm-ng.conf > $BWM_DIR/$N/client0.csv 2>&1 &
+ssh host81 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor0.csv 2>&1 &"
+ssh host82 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor1.csv 2>&1 &"
+ssh host83 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor2.csv 2>&1 &"
+ssh host85 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/proposer0.csv 2>&1 &"
+            nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/client0.csv 2>&1 &
 
-for ((i=1; i<=N; i++ ))
-do
-  c=$[$i+1]
-  ssh node$c "nohup $EXEC_DIR/learner $i $EXEC_DIR/paxos.conf > experiment_log/learner$i.log 2>&1 &"
-  ssh node$c "nohup bwm-ng $EXEC_DIR/bwm-ng.conf > $BWM_DIR/$N/learner$i.csv 2>&1 &"
-done
+ssh host88 "nohup $EXEC_DIR/learner 1 $CONF_DIR/paxos.conf > experiment_log/learner1.log 2>&1 &"
+ssh host88 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/learner1.csv 2>&1 &"
 
 #run the client
-$EXEC_DIR/client $EXEC_DIR/paxos.conf 0 $OST 8192 0 
+$EXEC_DIR/client $CONF_DIR/paxos.conf 0 $OST 8192 0 
 mv client0-$OST-8192B.csv udp-$N-$OST.csv
 
 echo "Terminate Acceptors and Proposers"
 
-ssh node70 "pkill acceptor"
-ssh node71 "pkill acceptor"
-ssh node72 "pkill acceptor"
-ssh node74 "pkill proposer"
-for ((i=1; i<=N; i++ ))
-  do
-  c=$[$i+1]
-  ssh node$c "pkill learner"
-  done
+ssh host81 "pkill acceptor"
+ssh host82 "pkill acceptor"
+ssh host83 "pkill acceptor"
+ssh host85 "pkill proposer"
+ssh host88 "pkill learner"
+#for ((i=1; i<=N; i++ ))
+#  do
+#  c=$[$i+1]
+#  ssh host$c "pkill learner"
+#  done
 echo "Experiment end successfully"

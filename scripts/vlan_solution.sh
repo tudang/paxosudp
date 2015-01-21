915c2560a08ff65021388435ed906f79194081d7
Tu Dang <danghu@usi.ch>
2015-01-21 17:44:21 +0100

    worked with 3 learners (node80, node85,node88), client(node86)

--- scripts/vlan_solution.sh
+++ scripts/vlan_solution.sh
@@ -11,9 +11,11 @@ ovs-vsctl add-port br0 ge-1/1/7 vlan_mode=access tag=2 -- set Interface ge-1/1/7
 ovs-vsctl add-port br0 ge-1/1/9 vlan_mode=access tag=3 -- set Interface ge-1/1/9 type=pica8
 ovs-vsctl add-port br0 ge-1/1/21 vlan_mode=trunk trunks=2,3 -- set Interface ge-1/1/21 type=pica8
 ovs-vsctl add-port br0 ge-1/1/22 vlan_mode=trunk trunks=2,4 -- set Interface ge-1/1/22 type=pica8
+ovs-vsctl add-port br0 ge-1/1/20 vlan_mode=trunk trunks=2,5 -- set Interface ge-1/1/20 type=pica8
 ovs-vsctl add-port br0 ge-1/1/31 vlan_mode=trunk trunks=2,3 -- set Interface ge-1/1/31 type=pica8
 ovs-vsctl add-port br0 ge-1/1/32 vlan_mode=trunk trunks=2,4 -- set Interface ge-1/1/32 type=pica8
-ovs-ofctl add-group br0 group_id=1,type=all,bucket=output:7,bucket=output:31,bucket=output:32
+ovs-vsctl add-port br0 ge-1/1/30 vlan_mode=trunk trunks=2,5 -- set Interface ge-1/1/30 type=pica8
+ovs-ofctl add-group br0 group_id=1,type=all,bucket=output:7,bucket=output:30,bucket=output:31,bucket=output:32
 ovs-ofctl add-flows br0 flows
 ovs-ofctl dump-groups-desc br0
 ovs-ofctl dump-flows br0

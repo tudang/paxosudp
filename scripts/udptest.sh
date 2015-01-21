b18899bcb168487793e79a0bf08bf8c0571eae0c
Tu Dang <danghu@usi.ch>
2015-01-21 14:34:52 +0100

    paxos with openflow duplication and multiplication

--- scripts/udptest.sh
+++ scripts/udptest.sh
@@ -12,20 +12,20 @@ BWM_DIR="/home/danghu/log-bwm"
 EXEC_DIR="/home/danghu/paxosudp/build/sample"
 CONF_DIR="/home/danghu/paxosudp/scripts"
 #start acceptors & proposer
-ssh host81 "nohup $EXEC_DIR/acceptor 0 $CONF_DIR/paxos.conf > experiment_log/acceptor0.log 2>&1 &"
-ssh host82 "nohup $EXEC_DIR/acceptor 1 $CONF_DIR/paxos.conf > experiment_log/acceptor1.log 2>&1 &"
-ssh host83 "nohup $EXEC_DIR/acceptor 2 $CONF_DIR/paxos.conf > experiment_log/acceptor2.log 2>&1 &"
-ssh host85 "nohup $EXEC_DIR/proposer 0 $CONF_DIR/paxos.conf > experiment_log/proposer0.log 2>&1 &"
+ssh node81 "nohup $EXEC_DIR/acceptor 0 $CONF_DIR/paxos.conf > experiment_log/acceptor0.log 2>&1 &"
+ssh node82 "nohup $EXEC_DIR/acceptor 1 $CONF_DIR/paxos.conf > experiment_log/acceptor1.log 2>&1 &"
+ssh node83 "nohup $EXEC_DIR/acceptor 2 $CONF_DIR/paxos.conf > experiment_log/acceptor2.log 2>&1 &"
+ssh node84 "nohup $EXEC_DIR/proposer 0 $CONF_DIR/paxos.conf > experiment_log/proposer0.log 2>&1 &"
 
 #start bwm-ng on acceptors & proposer
-ssh host81 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor0.csv 2>&1 &"
-ssh host82 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor1.csv 2>&1 &"
-ssh host83 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor2.csv 2>&1 &"
-ssh host85 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/proposer0.csv 2>&1 &"
+ssh node81 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor0.csv 2>&1 &"
+ssh node82 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor1.csv 2>&1 &"
+ssh node83 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/acceptor2.csv 2>&1 &"
+ssh node84 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/proposer0.csv 2>&1 &"
             nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/client0.csv 2>&1 &
 
-ssh host88 "nohup $EXEC_DIR/learner 1 $CONF_DIR/paxos.conf > experiment_log/learner1.log 2>&1 &"
-ssh host88 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/learner1.csv 2>&1 &"
+#ssh node88 "nohup $EXEC_DIR/learner 0 $CONF_DIR/paxos.conf > experiment_log/learner0.log 2>&1 &"
+#ssh node88 "nohup bwm-ng $CONF_DIR/bwm-ng.conf > $BWM_DIR/$N/learner0.csv 2>&1 &"
 
 #run the client
 $EXEC_DIR/client $CONF_DIR/paxos.conf 0 $OST 8192 0 
@@ -33,14 +33,14 @@ mv client0-$OST-8192B.csv udp-$N-$OST.csv
 
 echo "Terminate Acceptors and Proposers"
 
-ssh host81 "pkill acceptor"
-ssh host82 "pkill acceptor"
-ssh host83 "pkill acceptor"
-ssh host85 "pkill proposer"
-ssh host88 "pkill learner"
+ssh node81 "pkill acceptor"
+ssh node82 "pkill acceptor"
+ssh node83 "pkill acceptor"
+ssh node84 "pkill proposer"
+#ssh node88 "pkill learner"
 #for ((i=1; i<=N; i++ ))
 #  do
 #  c=$[$i+1]
-#  ssh host$c "pkill learner"
+#  ssh node$c "pkill learner"
 #  done
 echo "Experiment end successfully"
---
+++ scripts/vlan_solution.sh
@@ -0,0 +1,21 @@
+# ovs script to configure network on sw222
+ovs-vsctl del-br br0
+ovs-vsctl add-br br0 -- set bridge br0 datapath_type=pica8
+ovs-vsctl add-port br0 ge-1/1/1 vlan_mode=access tag=2 -- set Interface ge-1/1/1 type=pica8
+ovs-vsctl add-port br0 ge-1/1/2 vlan_mode=access tag=2 -- set Interface ge-1/1/2 type=pica8
+ovs-vsctl add-port br0 ge-1/1/3 vlan_mode=access tag=2 -- set Interface ge-1/1/3 type=pica8
+ovs-vsctl add-port br0 ge-1/1/4 vlan_mode=access tag=2 -- set Interface ge-1/1/4 type=pica8
+ovs-vsctl add-port br0 ge-1/1/5 vlan_mode=access tag=2 -- set Interface ge-1/1/5 type=pica8
+ovs-vsctl add-port br0 ge-1/1/6 vlan_mode=access tag=2 -- set Interface ge-1/1/6 type=pica8
+ovs-vsctl add-port br0 ge-1/1/7 vlan_mode=access tag=2 -- set Interface ge-1/1/7 type=pica8
+ovs-vsctl add-port br0 ge-1/1/8 vlan_mode=access tag=4 -- set Interface ge-1/1/8 type=pica8
+ovs-vsctl add-port br0 ge-1/1/9 vlan_mode=access tag=3 -- set Interface ge-1/1/9 type=pica8
+ovs-vsctl add-port br0 ge-1/1/20 vlan_mode=trunk trunks=2,4 -- set Interface ge-1/1/20 type=pica8
+ovs-vsctl add-port br0 ge-1/1/30 vlan_mode=trunk trunks=2,4 -- set Interface ge-1/1/30 type=pica8
+ovs-vsctl add-port br0 ge-1/1/21 vlan_mode=trunk trunks=2,3 -- set Interface ge-1/1/21 type=pica8
+ovs-vsctl add-port br0 ge-1/1/31 vlan_mode=trunk trunks=2,3 -- set Interface ge-1/1/31 type=pica8
+ovs-vsctl add-port br0 ge-1/1/45 vlan_mode=trunk trunks=2,3 -- set Interface ge-1/1/45 type=pica8
+ovs-ofctl add-group br0 group_id=1,type=all,bucket=output:7,bucket=output:30,bucket=output:31
+ovs-ofctl add-flows br0 flows
+ovs-ofctl dump-groups-desc br0
+ovs-ofctl dump-flows br0

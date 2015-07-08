b0d434ad7e1d240169d3e1a4520a74228522f9a8
Tu Dang <danghu@usi.ch>
2015-07-08 16:25:33 +0200

    remove unwanted files

--- lib/msgpack-c/CMakeFiles/doxygen_c.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/doxygen_c"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/doxygen_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- lib/msgpack-c/CMakeFiles/doxygen_cpp.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/doxygen_cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/doxygen_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
